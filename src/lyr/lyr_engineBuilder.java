23a91909ddfa25bad5e4223f9cf9d6f14e87ccba
lyravega <alphalyr.vega@gmail.com>
2022-02-24 02:10:32 +0300

    rename

--- src/lyr/lyr_engineBuilder.java
+++ src/lyr/lyr_engineBuilder.java
@@ -4,17 +4,17 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
-import com.fs.starfarer.api.combat.EngineSlotAPI;
-
 /**
  * A proxy-like class for... engine builder? I have no idea what 
  * this is. As far as I can tell, there is no API equalivent to
  * whatever {@code getEngineSlots()} retrieves.
  * <p> The retrieved objects have EngineSlot data in them, but
  * there is a method that formats all of those slots, hence my 
- * reason calling this a 'builder', which probably formats the 
- * engines according to their 'style' enum,and sets them on the 
- * ship.
+ * reason calling this a 'builder'. There is a method with 
+ * several 'style enums' that keeps all the data, with the last
+ * enum being 'CUSTOM' and grabs stuff from the SpecStore. 
+ * Accessing them is... meh, I won't even try, to be honest. 
+ * Future you (me), just don't. 
  * <p> Accessing the EngineSlot data directly, and trying to 
  * alter anything has no visible results as far as I can tell. 
  * There also is an EngineSlotAPI, and I was expecting a relation
@@ -33,49 +33,65 @@ import com.fs.starfarer.api.combat.EngineSlotAPI;
  * @version 0.7
  * @since 0.7
  */
-public class lyr_engineSlot {
+public class lyr_engineBuilder {
 	private Object engineSlot;
 	private static final Class<?> obfuscatedEngineBuilderClass = _lyr_finder.obfuscatedEngineBuilderClass;
 	private static final Class<?> obfuscatedEngineStyleEnum = _lyr_finder.obfuscatedEngineStyleEnum;
 	private static final String obfuscatedEngineStyleSetterName = _lyr_finder.obfuscatedEngineStyleSetterName;
+
+	public static enum engineStyle { ;
+		public static final int lowTech = 0;
+		public static final int midline = 1;
+		public static final int highTech = 2;
+		// public static final int cobraBomber = 3; // nothing
+		// public static final int lowTechMissile = 4;
+		// public static final int midlineMissile = 5;
+		// public static final int highTechMissile = 6;
+		// public static final int lowTechFighter = 7;
+		public static final int torpedo = 8; // pretty
+		// public static final int torpedoAtropos = 9; // ugly
+		// public static final int lowTechRocket = 10; // weird cut-off
+		// public static final int doritos = 11; // nothing
+		public static final int custom = 12;
+	}
 	
 	/**
-	 * Creates a new instance for the passed {@link EngineSlotAPI}, and 
+	 * Creates a new instance for the passed {@link Object}, and 
 	 * clones it if necessary. Alterations should be done on a clone if 
 	 * it is going to be a new slot.
 	 * @param engineSlot to be proxied
 	 * @param clone if the engineSlot needs to be cloned
 	 */
-	public lyr_engineSlot(Object engineSlot, boolean clone) {
+	public lyr_engineBuilder(Object engineSlot, boolean clone) {
 		this.engineSlot = (clone) ? this.duplicate(engineSlot) : engineSlot;
 	}
 	
 	/**
-	 * Used to retrieve the stored {@link EngineSlotAPI} in the proxy to
+	 * Used to retrieve the stored {@link Object} in the proxy to
 	 * access the API methods through the proxy itself, or to use it if
 	 * it needs to be applied on something.
-	 * @return the stored {@link EngineSlotAPI}
+	 * @return the stored {@link Object}
 	 */
 	public Object retrieve() {
 		return engineSlot;
 	}
 	
 	/**
-	 * Used to exchange the {@link EngineSlotAPI} stored in the proxy
+	 * Used to exchange the {@link Object} stored in the proxy
 	 * class in order to re-use this proxy instead of creating new ones.
 	 * @param engineSlot to exchange with the stored one
 	 * @return the proxy itself for chaining purposes
 	 */
-	public lyr_engineSlot recycle(Object engineSlot) {
+	public lyr_engineBuilder recycle(Object engineSlot) {
 		this.engineSlot = engineSlot;
 		return this;
 	}
 
 	/**
-	 * Clones the stored {@link EngineSlotAPI}, and returns it. For 
+	 * Clones the stored {@link Object}, and returns it. For 
 	 * internal use if necessary. {@link #retrieve()} should be used
 	 * if access to the API is needed.
-	 * @return a cloned {@link EngineSlotAPI}
+	 * @return a cloned {@link Object}
 	 */
 	protected Object duplicate(Object engineSlot) {
 		try {
@@ -88,8 +104,8 @@ public class lyr_engineSlot {
 	
 	//#region API-like methods
 	@Override
-	public lyr_engineSlot clone() {
-		return new lyr_engineSlot(engineSlot, true);
+	public lyr_engineBuilder clone() {
+		return new lyr_engineBuilder(engineSlot, true);
 	}
 	
 	public void setEngineStyle(int enumNumber) {
