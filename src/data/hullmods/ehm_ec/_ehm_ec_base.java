3b90d941497a1afa99f2bb6b8a050f061dbf2580
lyravega <alphalyr.vega@gmail.com>
2022-02-24 02:13:27 +0300

    engine styles!

---
+++ src/data/hullmods/ehm_ec/_ehm_ec_base.java
@@ -0,0 +1,55 @@
+package data.hullmods.ehm_ec;
+
+import java.util.Set;
+
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipHullSpecAPI;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
+
+import data.hullmods._ehm_base;
+import data.hullmods.ehm_ar._ehm_ar_base;
+import data.hullmods.ehm_sr._ehm_sr_base;
+import lyr.lyr_engineBuilder;
+import lyr.lyr_hullSpec;
+
+/**
+ * This class is used by weapon retrofit hullmods. They are pretty 
+ * straightforward in their operation; change all of the weapon slots 
+ * on a ship to a different type. 
+ * @see {@link _ehm_ar_base} for slot adapter base
+ * @see {@link _ehm_sr_base} for system retrofit base
+ * @author lyravega
+ * @version 0.7
+ * @since 0.3
+ */
+public class _ehm_ec_base extends _ehm_base {
+	protected static final ShipHullSpecAPI ehm_pimpMyEngineSlots(ShipVariantAPI variant, int styleEnum) {
+		lyr_hullSpec hullSpec = new lyr_hullSpec(variant.getHullSpec(), false);
+		lyr_engineBuilder engineSlot = new lyr_engineBuilder(null, false);
+
+		for (Object temp : hullSpec.getEngineSlots()) {
+			engineSlot.recycle(temp).setEngineStyle(styleEnum);
+		}
+
+		return hullSpec.retrieve();
+	}
+
+	public static final ShipHullSpecAPI ehm_restoreEngineSlots(ShipVariantAPI variant) {
+		return ehm_hullSpecRestore(variant);
+	}
+
+	//#region INSTALLATION CHECKS
+	@Override
+	protected String unapplicableReason(ShipAPI ship) {
+		if (ship == null) return ehm.excuses.noShip; 
+
+		if (!ehm_hasRetrofitBaseBuiltIn(ship)) return ehm.excuses.lacksBase; 
+		if (ehm_hasRetrofitTag(ship, ehm.tag.shieldCosmetic, hullModSpecId)) return ehm.excuses.hasShieldCosmetic;
+
+		Set<String> hullModSpecTags = hullModSpec.getTags();
+		if (hullModSpecTags.contains(ehm.tag.reqShields) && ship.getShield() == null) return ehm.excuses.noShields;
+
+		return null; 
+	}
+	//#endregion
+}
---
+++ src/data/hullmods/ehm_ec/ehm_ec_torpedoEngines.java
@@ -0,0 +1,23 @@
+package data.hullmods.ehm_ec;
+
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
+
+import lyr.lyr_engineBuilder.engineStyle;
+
+/**@category Engine Cosmetic 
+ * @author lyravega
+ * @version 0.7
+ * @since 0.6
+ */
+public class ehm_ec_torpedoEngines extends _ehm_ec_base {
+	private static final int style = engineStyle.torpedo;
+
+	@Override
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String hullModSpecId) {
+		ShipVariantAPI variant = stats.getVariant();
+		
+		variant.setHullSpecAPI(ehm_pimpMyEngineSlots(variant, style));
+	}
+}
