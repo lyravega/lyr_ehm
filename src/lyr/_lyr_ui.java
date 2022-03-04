5617daf64c25f5924ff2afb3b0fd0e0394ebf1f8
lyravega <alphalyr.vega@gmail.com>
2022-03-04 19:43:42 +0300

    'recursiveChildrenSearch()' touch-up

--- src/lyr/_lyr_ui.java
+++ src/lyr/_lyr_ui.java
@@ -4,14 +4,11 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.invoke.MethodType;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.CampaignUIAPI;
-import com.fs.starfarer.api.combat.ShipSystemSpecAPI;
-import com.fs.starfarer.api.combat.ShipVariantAPI;
-import com.fs.starfarer.api.fleet.FleetMemberAPI;
 
 import data.hullmods.ehm_base;
 
@@ -66,27 +63,28 @@ public class _lyr_ui {
 		return targetObject;
 	}
 
-	private static Object recursiveChildrenSearch(Object object, String methodName) {
-		Object targetObject = null;
+	private static List<Object> objectPath = new LinkedList<Object>();
 
-		try {
+	private static Object recursiveChildrenSearch(Object object, String methodName) {
+		try { // search the current object for the methodName
+			Class<?> methodClass = Class.forName("java.lang.reflect.Method", false, Class.class.getClassLoader());
+			MethodHandle getDeclaredMethod = MethodHandles.lookup().findVirtual(Class.class, "getDeclaredMethod", MethodType.methodType(methodClass, String.class, Class[].class));
+			if (getDeclaredMethod.invoke(object.getClass(), methodName) != null) return object;
+		} catch (Throwable t) {
+			// no catch, fuck you
+		}
+
+		try { // if not found, search children of the object recursively
 			MethodHandle getChildrenCopy = MethodHandles.lookup().findVirtual(object.getClass(), "getChildrenCopy", MethodType.methodType(List.class));
 			List<?> children = List.class.cast(getChildrenCopy.invoke(object));
 
-			for (Object child : children) {
-				for (Object method : child.getClass().getDeclaredMethods())
-					if (method.toString().contains(methodName)) return child;
-			}
-	
-			for (Object child : children) {
-				targetObject = recursiveChildrenSearch(child, methodName);
-				if (targetObject != null) return targetObject; 
-			}
+			for (Object child : children) 
+				if (recursiveChildrenSearch(child, methodName) != null) return child; 
 		} catch (Throwable t) {
-			//
+			// no catch, fuck you twice
 		}
 
-		return targetObject;
+		return null;
 	}
 
 	public static void refreshRefitShip() {
@@ -94,18 +92,15 @@ public class _lyr_ui {
 			findCoreClass();
 			findRefitPanelClass();
 
-			Class<?> fieldClass = Class.forName("java.lang.reflect.Field", false, Class.class.getClassLoader());
-			Class<?> methodClass = Class.forName("java.lang.reflect.Method", false, Class.class.getClassLoader());
-
 			Map<String, Object> getDialogParentMap = _lyr_finder.findTypesForMethod(Global.getSector().getCampaignUI().getClass(), "getDialogParent");
 			Object screenPanel = MethodHandle.class.cast(getDialogParentMap.get("methodHandle")).invoke(Global.getSector().getCampaignUI());
 
-			Object refitPanel = recursiveChildrenSearch(screenPanel, refitPanelClass);
+			Object refitPanel = recursiveChildrenSearch(screenPanel, refitPanelClass); // TODO: find proper path
 
 			MethodHandle saveCurrentVariant = MethodHandles.lookup().findVirtual(refitPanelClass, "saveCurrentVariant", MethodType.methodType(void.class, boolean.class));
-			saveCurrentVariant.invoke(refitPanel, true);
+			saveCurrentVariant.invoke(refitPanel, false);
 
-			Object button = recursiveChildrenSearch(refitPanel, "undo");
+			Object button = recursiveChildrenSearch(refitPanel, "undo"); // TODO: find proper path
 
 			Map<String, Object> undoMap = _lyr_finder.findTypesForMethod(button.getClass(), "undo");
 			MethodHandle.class.cast(undoMap.get("methodHandle")).invoke(button);
