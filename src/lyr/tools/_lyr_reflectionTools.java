90223d919a35ec60b81b91b457a5dbcbf307bb61
lyravega <alphalyr.vega@gmail.com>
2022-03-05 18:16:15 +0300

    change map to class, why even use a map

--- src/lyr/tools/_lyr_reflectionTools.java
+++ src/lyr/tools/_lyr_reflectionTools.java
@@ -4,9 +4,8 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.invoke.MethodType;
-import java.util.HashMap;
-import java.util.Map;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 
@@ -28,8 +27,10 @@ public class _lyr_reflectionTools {
 	protected static MethodHandle getParameterTypes;
 	protected static MethodHandle getReturnType;
 	protected static MethodHandle getDeclaredMethod;
+	protected static MethodHandle getMethod;
 
 	static {
+		logger.setLevel(Level.INFO);
 		try {
 			fieldClass = Class.forName("java.lang.reflect.Field", false, Class.class.getClassLoader());
 			methodClass = Class.forName("java.lang.reflect.Method", false, Class.class.getClassLoader());
@@ -37,9 +38,29 @@ public class _lyr_reflectionTools {
 			getParameterTypes = lookup.findVirtual(methodClass, "getParameterTypes", MethodType.methodType(Class[].class));
 			getReturnType = lookup.findVirtual(methodClass, "getReturnType", MethodType.methodType(Class.class));
 			getDeclaredMethod = lookup.findVirtual(Class.class, "getDeclaredMethod", MethodType.methodType(methodClass, String.class, Class[].class));
+			getMethod = lookup.findVirtual(Class.class, "getMethod", MethodType.methodType(methodClass, String.class, Class[].class));
 		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected static class methodMap {
+		private Class<?> returnType;
+		private Class<?>[] parameterTypes;
+		private MethodType methodType;
+		private MethodHandle methodHandle;
+
+		protected methodMap(Class<?> returnType, Class<?>[] parameterTypes, MethodType methodType, MethodHandle methodHandle) {
+			this.returnType = returnType;
+			this.parameterTypes = parameterTypes;
+			this.methodType = methodType;
+			this.methodHandle = methodHandle;
+		}
+
+		protected Class<?> getReturnType() { return this.returnType; }
+		protected Class<?>[] getParameterTypes() { return this.parameterTypes; }
+		protected MethodType getMethodType() { return this.methodType; }
+		protected MethodHandle getMethodHandle() { return this.methodHandle; }
 	}
 
 	/**
@@ -51,18 +72,29 @@ public class _lyr_reflectionTools {
 	 * @return a map with "returnType", "parameterTypes", "methodType" and "methodHandle" keys
 	 * @throws Throwable 
 	 */
-	public static Map<String, Object> findTypesForMethod(Class<?> clazz, String methodName) throws Throwable {
-		Object method = getDeclaredMethod.invoke(clazz, methodName);
-		Class<?>[] parameterTypes = (Class<?>[]) getParameterTypes.invoke(method);
+	public static methodMap findTypesForMethod(Class<?> clazz, String methodName) throws Throwable {
+		return findTypesForMethod(clazz, methodName, true);
+	}
+	public static methodMap findTypesForMethod(Class<?> clazz, String methodName, boolean useDeclared) throws Throwable {
+		Object method = null;
+		try { // works for methods with no arguments; if there's a way to capture it through a MethodHandle, I'm not aware of it
+			method = (useDeclared) ? getDeclaredMethod.invoke(clazz, methodName) : getMethod.invoke(clazz, methodName);
+		} catch (Throwable t) { // fallback for the problem above, searches all the methods with the passed name, and uses the FIRST found one
+			for (Object currMethod : (useDeclared) ? clazz.getDeclaredMethods() : clazz.getMethods()) {
+				if (!String.class.cast(getName.invoke(currMethod)).equals(methodName)) continue;
+
+				method = currMethod; break;
+			}
+		}
+
+		if (method == null) throw new Throwable("Declared method with the name '"+methodName+"' not found in the class '"+clazz.getName()+"'");
+
 		Class<?> returnType = (Class<?>) getReturnType.invoke(method);
+		Class<?>[] parameterTypes = (Class<?>[]) getParameterTypes.invoke(method);
 
 		MethodType methodType = MethodType.methodType(returnType, parameterTypes);
 		MethodHandle methodHandle = MethodHandles.lookup().findVirtual(clazz, methodName, MethodType.methodType(returnType, parameterTypes));
-		Map<String, Object> typeMap = new HashMap<String, Object>();
-		typeMap.put("returnType", returnType);
-		typeMap.put("parameterTypes", parameterTypes);
-		typeMap.put("methodType", methodType);
-		typeMap.put("methodHandle", methodHandle);
-		return typeMap;
+
+		return new methodMap(returnType, parameterTypes, methodType, methodHandle);
 	}
 }
