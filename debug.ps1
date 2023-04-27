fa68ce6fa23eb7ca4e9725eddd0d2109aef7e2d3
lyravega <alphalyr.vega@gmail.com>
2023-04-27 10:30:10 +0300

    y u no work

--- debug.ps1
+++ debug.ps1
@@ -1,5 +1,3 @@
-$coreFolder = ../../starsector-core
-
-Set-Location $coreFolder
+Set-Location ../../starsector-core
 ../jre/bin/java.exe "-agentlib:jdwp=transport=dt_socket,address=8888,server=y,suspend=y" "-Dorg.codehaus.janino.source_debugging.enable=true" "-Djava.library.path=native\windows" -Xms4096m -Xmx4096m -Xss2048k "-Dcom.fs.starfarer.settings.paths.logs=." "-Dcom.fs.starfarer.settings.paths.saves=../saves" "-Dcom.fs.starfarer.settings.paths.screenshots=../screenshots" "-Dcom.fs.starfarer.settings.paths.mods=../mods" -classpath "janino.jar;commons-compiler.jar;commons-compiler-jdk.jar;starfarer.res.jar;starfarer.api.jar;starfarer_obf.jar;jogg-0.0.7.jar;jorbis-0.0.15.jar;json.jar;lwjgl.jar;lwjgl_util_applet.jar;jinput.jar;lwjgl_test.jar;log4j-1.2.9.jar;lwjgl_util.jar;fs.sound_obf.jar;fs.common_obf.jar;xstream-1.4.10.jar" com.fs.starfarer.StarfarerLauncher
 Set-Location $PSScriptRoot
