72ec6b816ba3564c62df9066e3b109371a617340
lyravega <alphalyr.vega@gmail.com>
2023-05-17 19:44:22 +0300

    ps shit

--- pack.ps1
+++ pack.ps1
@@ -1,22 +1,24 @@
-$stuffToPack =  (".\LICENSE.md"),
-                (".\README.md"),
-                (".\mod_info.json"),
-                (".\EHM.version"),
-                (".\jars"),
-                (".\data"),
-                (".\customization")
+$stuffToPack =  ("LICENSE.md"),
+                ("README.md"),
+                ("mod_info.json"),
+                ("EHM.version"),
+                ("jars"),
+                ("data"),
+                ("customization")
 
 foreach($line in Get-Content ".\mod_info.json") {
-    if($line -match '"version"'){
-        $line -match ':"(.*?)",'
+    if($line -match '"version"') {
+        $null = $line -match ':"(.*?)",'
         $version = $Matches[1]
-        Write-Host $version
     }
 }
 
+$null = New-Item "$PSScriptRoot\lyr_ehm\" -ItemType Directory
 foreach($path in $stuffToPack)
 {
-    Write-Host $path -ForegroundColor Red
-    Compress-Archive -Path $path -DestinationPath "$PSScriptRoot\lyr_ehm $version.zip" -Update
+    Write-Host $path -ForegroundColor Blue
+    Copy-Item -Path ".\$path" -Destination "$PSScriptRoot\lyr_ehm\" -Recurse
 }
+Compress-Archive -Path "$PSScriptRoot\lyr_ehm\" -DestinationPath "$PSScriptRoot\lyr_ehm $version.zip" -Update
+Remove-Item "$PSScriptRoot\lyr_ehm\" -Force -Recurse
 Write-Host "Mod packed!" -ForegroundColor Green
