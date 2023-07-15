423a859e3f39d5646320fba8ab79924ea7271045
lyravega <alphalyr.vega@gmail.com>
2023-07-15 18:14:50 +0300

    extension self-removal just in case

--- src/experimentalHullModifications/hullmods/ehm_mr/ehm_mr_expensivemissiles.java
+++ src/experimentalHullModifications/hullmods/ehm_mr/ehm_mr_expensivemissiles.java
@@ -1,11 +1,16 @@
 package experimentalHullModifications.hullmods.ehm_mr;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Stats;
+import com.fs.starfarer.api.util.DynamicStatsAPI;
 
 import experimentalHullModifications.hullmods.ehm_wr.ehm_wr_missileslotretrofit;
+import lyravega.listeners.events.normalEvents;
+import lyravega.misc.lyr_internals;
 
 /**
  * @category Effect Extension
@@ -13,11 +18,25 @@ import experimentalHullModifications.hullmods.ehm_wr.ehm_wr_missileslotretrofit;
  * @author lyravega
  */
 public class ehm_mr_expensivemissiles extends BaseHullMod {
+	private static final String masterHullModId = lyr_internals.id.hullmods.missileslotretrofit;
+
 	@Override
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
-		stats.getDynamic().getMod(Stats.SMALL_MISSILE_MOD).modifyFlat(id, 2);
-		stats.getDynamic().getMod(Stats.MEDIUM_MISSILE_MOD).modifyFlat(id, 4);
-		stats.getDynamic().getMod(Stats.LARGE_MISSILE_MOD).modifyFlat(id, 8);
+		if (!checkSource(stats.getVariant(), masterHullModId)) return;
+
+		DynamicStatsAPI dynamicStats = stats.getDynamic();
+		dynamicStats.getMod(Stats.SMALL_MISSILE_MOD).modifyFlat(id, 2);
+		dynamicStats.getMod(Stats.MEDIUM_MISSILE_MOD).modifyFlat(id, 4);
+		dynamicStats.getMod(Stats.LARGE_MISSILE_MOD).modifyFlat(id, 8);
+	}
+
+	private boolean checkSource(ShipVariantAPI variant, String sourceId) {
+		if (!variant.hasHullMod(sourceId)) {
+			normalEvents hullModEffect = (normalEvents) Global.getSettings().getHullModSpec(sourceId).getEffect();
+			hullModEffect.onRemove(variant);
+			return false;
+		}
+		return true;
 	}
 
 	@Override
--- src/experimentalHullModifications/hullmods/ehm_mr/ehm_mr_heavyenergyintegration.java
+++ src/experimentalHullModifications/hullmods/ehm_mr/ehm_mr_heavyenergyintegration.java
@@ -2,12 +2,16 @@ package experimentalHullModifications.hullmods.ehm_mr;
 
 import static com.fs.starfarer.api.impl.hullmods.HeavyBallisticsIntegration.COST_REDUCTION;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Stats;
 
 import experimentalHullModifications.hullmods.ehm_wr.ehm_wr_energyslotretrofit;
+import lyravega.listeners.events.normalEvents;
+import lyravega.misc.lyr_internals;
 
 /**
  * @category Effect Extension 
@@ -15,9 +19,22 @@ import experimentalHullModifications.hullmods.ehm_wr.ehm_wr_energyslotretrofit;
  * @author lyravega
  */
 public class ehm_mr_heavyenergyintegration extends BaseHullMod {
+	private static final String masterHullModId = lyr_internals.id.hullmods.energyslotretrofit;
+
 	@Override
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		if (!checkSource(stats.getVariant(), masterHullModId)) return;
+
 		stats.getDynamic().getMod(Stats.LARGE_ENERGY_MOD).modifyFlat(id, -COST_REDUCTION);
+	}
+
+	private boolean checkSource(ShipVariantAPI variant, String sourceId) {
+		if (!variant.hasHullMod(sourceId)) {
+			normalEvents hullModEffect = (normalEvents) Global.getSettings().getHullModSpec(sourceId).getEffect();
+			hullModEffect.onRemove(variant);
+			return false;
+		}
+		return true;
 	}
 	
 	@Override
