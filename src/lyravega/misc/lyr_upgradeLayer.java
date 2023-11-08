71a4115276b544610dadcae84bc3b2291c3d5a9e
lyravega <alphalyr.vega@gmail.com>
2023-11-09 01:09:41 +0300

    you little shit

--- src/lyravega/misc/lyr_upgradeLayer.java
+++ src/lyravega/misc/lyr_upgradeLayer.java
@@ -74,6 +74,11 @@ public class lyr_upgradeLayer {
 			case 3: return " III";
 			case 4: return " IV";
 			case 5: return " V";
+			case 6: return " VI";
+			case 7: return " VII";
+			case 8: return " VIII";
+			case 9: return " IX";
+			case 10: return " X";
 			default: return " "+num;
 		}
 	}
@@ -84,7 +89,6 @@ public class lyr_upgradeLayer {
 	 * @return {@code true} if it may be afforded, {@code false} otherwise
 	 */
 	public boolean canAfford() {
-
 		if (this.storyPointCost > 0) {
 			final MutableCharacterStatsAPI playerStats = Global.getSector().getPlayerStats();
 
