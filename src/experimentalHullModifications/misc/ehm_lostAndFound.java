07d97c30ffede0b45f0eb0cd405d144e7c15bc64
lyravega <alphalyr.vega@gmail.com>
2024-01-09 08:17:46 +0300

    NPE fix

--- src/experimentalHullModifications/misc/ehm_lostAndFound.java
+++ src/experimentalHullModifications/misc/ehm_lostAndFound.java
@@ -21,12 +21,14 @@ public class ehm_lostAndFound {
 	}
 
 	public static void returnStuff() {
+		lyr_logger.warn("Returning stashed weapons to player cargo");
+		Global.getSector().getCampaignUI().addMessage("EHM (Experimental Hull Modifications): Load successful. Please save the game and disable the 'Clear Unknown Slots' debug option. Removed weapons are returned to your cargo", Color.RED);
+
+		if (recoveryStash == null || !recoveryStash.isEmpty()) return;
+
 		for (Entry<String, Integer> weaponEntry : recoveryStash.entrySet()) {
 			Global.getSector().getPlayerFleet().getCargo().addWeapons(weaponEntry.getKey(), weaponEntry.getValue());
 		}
-
-		lyr_logger.warn("Returning stashed weapons to player cargo");
-		Global.getSector().getCampaignUI().addMessage("EHM (Experimental Hull Modifications): Load successful. Please save the game and disable the 'Clear Unknown Slots' debug option. Removed weapons are returned to your cargo", Color.RED);
 
 		recoveryStash.clear(); recoveryStash = null;
 	}
